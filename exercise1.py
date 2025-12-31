fd16e9f523049bd47f58531371667dbcc0be312c
Erblin Syla <erblin.sylaa@gmail.com>
2025-12-31 15:04:10 +0100

    Adding files

---
+++ exercise1.py
@@ -0,0 +1,25 @@
+from fastapi import Body, FastAPI
+
+app = FastAPI()
+
+
+BOOKS = [
+    {'title': 'Title One', 'author': 'Author One', 'category': 'science'},
+    {'title': 'Title Two', 'author': 'Author Two', 'category': 'science'},
+    {'title': 'Title Three', 'author': 'Author Three', 'category': 'history'},
+    {'title': 'Title Four', 'author': 'Author Four', 'category': 'math'},
+    {'title': 'Title Five', 'author': 'Author Five', 'category': 'math'},
+    {'title': 'Title Six', 'author': 'Author Two', 'category': 'math'},
+]
+
+#1. Create a new API Endpoint that can fetch all books
+# from a specific author using either Path Parameters or Query Parameters.
+
+# PATH PARAMETERS WAY
+@app.get("/books/byauthor/{author}")
+async def read_books_by_author_path(author: str):
+    books_to_return = []
+    for book in BOOKS:
+        if book.get('author').casefold() == author.casefold():
+            books_to_return.append(book)
+    return books_to_return
